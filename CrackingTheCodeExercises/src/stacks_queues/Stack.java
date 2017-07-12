c8fcf2292d298b6445986195390ad9f5ca0d216d
Arthur Fernandes Araujo <arthurfernandes05@gmail.com>
2017-07-11 21:47:39 -0300

    Custom Stack Implementation using Java Generics

---
+++ CrackingTheCodeExercises/src/stacks_queues/Stack.java
@@ -0,0 +1,48 @@
+package stacks_queues;
+
+public class Stack<T>{
+	private Node<T> top;
+	
+	public Stack() {
+		top = null;
+	}
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public T peek() {
+		return top == null ? null : top.data;
+	}
+	
+	public void push(T data) {
+		final Node<T> node = new Node<>(data);
+		if(this.isEmpty()) {
+			top = node;
+		}
+		else {
+			node.next = top;
+			top = node;
+		}
+	}
+	
+	public T pop() {
+		if(this.isEmpty()) {
+			return null;
+		}
+		else {
+			T data = top.data;
+			top = top.next;
+			return data;
+		}
+	}
+	
+	public static class Node<T> {
+		public T data;
+		public Node<T> next;
+		public Node(T data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
