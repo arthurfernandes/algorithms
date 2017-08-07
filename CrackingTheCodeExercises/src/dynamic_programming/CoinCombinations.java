e2c7969900de07f0ab5fd401a65afaeb4a5aa262
Arthur Fernandes Araujo <arthurfernandes05@gmail.com>
2017-08-07 20:18:23 -0300

    Fixing bug in implementation of Cracking the Code problem 9.8

--- CrackingTheCodeExercises/src/dynamic_programming/CoinCombinations.java
+++ CrackingTheCodeExercises/src/dynamic_programming/CoinCombinations.java
@@ -6,34 +6,44 @@ package dynamic_programming;
 public class CoinCombinations {
 	
 	public static void main(String[] args) {
-		System.out.println(coinReps(25));
+		for(int i = 0; i <= 100; i++) {
+			System.out.println("Position: " + i);
+			System.out.println(coinReps(i));
+		}
 	}
 	
 	public static int coinReps(int n) {
-		if( n < 0) {
-			return 0;
+		if (n < 0) {
+			return -1;
 		}
 		else {
-			int[] cache = new int[n+1];
-			cache[0] = 1;
-			int[] coins = {1, 5, 10 , 25};
-			return coinRepsHelper(n, coins, cache);
+			int[] coins = {25, 10, 5, 1};
+			return makeChange(n, coins, 0);
 		}
 	}
-	
-	public static int coinRepsHelper(int n, int[] coins, int[] cache) {
-		if (n < 0)
-			return 0;
-		
-		if(cache[n] != 0)
-			return cache[n];
-		
-		int val = 0;
-		for(int coin : coins)
-			val += coinRepsHelper(n - coin, coins, cache);
-		
-		cache[n] = val;
-		return val;
-	}
 
+	public static int makeChange(int n, int[] coins, int curCoin) {
+		if (n == 0) {
+			return 1;
+		}
+		
+		int coin = coins[curCoin];
+		
+		if (curCoin == coins.length -1) { //On last coin, check if you can resolve
+			if (n % coin == 0) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		else {
+			int ways = 0;
+			for(int i = 0; i*coin <= n; i++) {
+				ways += makeChange(n - i*coin, coins, curCoin + 1);
+			}
+			
+			return ways;
+		}
+	}
 }
